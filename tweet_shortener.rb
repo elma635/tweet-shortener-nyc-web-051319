ad082831a03281efcd8571efe7f01e19c6af2f37
Elma Rahman <elmarahman635@gmail.com>
2019-04-22 04:59:05 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def dictionary(words_to_be_substituted)
+
+end
+
+
+
+def word_substituter("tweet")
+  "tweet".select do |full_word , sub_words|
+    full_word.to_s.split(",")
+
+  
+    if tweet < 140 characters
+
+tweet.keys
+
+
+
+
+
+
+
+
+  full_word.join(" ")
+  end
+end
