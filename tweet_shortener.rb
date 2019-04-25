e2fc2b85dab3aab65c2effec80007ba25b07f731
Elma Rahman <elmarahman635@gmail.com>
2019-04-25 20:02:31 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,17 +16,17 @@ def dictionary
     }
 end
 
+fullword = fullword.downcase to collectively incorporate the .downcase mthod instead of individually adding it.
+
 def word_substituter(tweet)
-  tweet.split.collect do |full_word|
-      if dictionary[full_word] #checking to see if full_word is inside hash
-         dictionary[full_word] #if it is, it will return it's value (strings will alwys be truthy values) #bc its not in the hash, no key for value and will be falsey which doesnt exist so it will move on the else 
+  tweet.split.collect do |full_word| #var used to refer to each element -- dont call anything on it.
+      if dictionary[full_word.downcase] #checking to see if full_word is inside hash
+         dictionary[full_word.downcase] #if it is, it will return it's value (strings will alwys be truthy values) #bc its not in the hash, no key for value and will be falsey which doesnt exist so it will move on the else
       else
         full_word #returns same word if its not inside hash.
        end
      end.join(" ")
   end
-
-
 
 
 
@@ -47,7 +47,7 @@ def selective_tweet_shortener(tweet)
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).chars.length > 140
-    truncate = word_substituter(tweet).chars[0..138] << ("...").join(" ")
+    truncate = word_substituter(tweet).chars[0..138] << ("...")
   else
     word_substituter(tweet)
   end
