692d02bfdbdabd87dd3c73d518e588eaa5099d59
Elma Rahman <elmarahman635@gmail.com>
2019-04-25 19:28:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ end
 def word_substituter(tweet)
   tweet.split.collect do |full_word|
       if dictionary[full_word] #checking to see if full_word is inside hash
-         dictionary[full_word] #if it is, it will return it's value (strings will alwys be truthy values) #bc its not in the hash, no key for value and will be falsey which doesnt exist so it will move on the else 
+         dictionary[full_word] #if it is, it will return it's value (strings will alwys be truthy values) #bc its not in the hash, no key for value and will be falsey which doesnt exist so it will move on the else
       else
         full_word #returns same word if its not inside hash.
        end
@@ -47,7 +47,7 @@ def selective_tweet_shortener(tweet)
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).chars.length > 140
-    truncate = word_substituter(tweet).chars[0..138] << ("...").join(" ")
+    truncate = word_substituter(tweet).chars[0..138] << ("...").join("")
   else
     word_substituter(tweet)
   end
